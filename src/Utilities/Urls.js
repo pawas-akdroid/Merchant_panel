b5f7532bd374a6d0ed93ec22f0b52901d341b8d4
gh0osty-Akdroid <101495769+gh0osty-Akdroid@users.noreply.github.com>
2023-03-16 02:39:42 +0545

    Update Url.js

--- src/Utilities/Urls.js
+++ src/Utilities/Urls.js
@@ -6,7 +6,7 @@ import axios from 'axios'
 
 
 export const MerchantUrl = axios.create({
-    baseURL: `http://44.237.37.194:6001/api/v1/merchant`,
+    baseURL: `http://44.232.151.192:6001/api/v1/merchant`,
   
 })  
 
@@ -18,18 +18,18 @@ export const MerchantTokenUrl =()=>{
    
     
     return axios.create({
-        baseURL: `http://44.237.37.194:6001/api/v1/merchant`,
+        baseURL: `http://44.232.151.192:6001/api/v1/merchant`,
         headers:{
             "Authorization":`Bearer ${token}`,
             "site" : site,
-            'Access-Control-Allow-Origin': ['http://44.237.37.194:6001/', "http://52.26.60.188:6969/"]
+            'Access-Control-Allow-Origin': ['http://44.232.151.192:6001/', "http://3.131.17.220:6969/"]
         }
       
     })
 } 
 
 
-export const  GameImgUrl ="http://52.26.60.188:6969/"
-export const  ImgUrl ="http://44.237.37.194:6001/"
+export const  GameImgUrl ="http://3.131.17.220:6969/"
+export const  ImgUrl ="http://44.232.151.192:6001/"
 export const  ImgUrl2 ="http://localhost:6001/"
 
